bdf95eebf3736af5ccaf3d0a35dc4934cd1dd09a
lOLO <laila.mf2005@gmail.com>
2025-06-30 14:08:10 +0300

    working on same problem final edit

--- frontend/src/contexts/AuthContext.js
+++ frontend/src/contexts/AuthContext.js
@@ -39,7 +39,7 @@ export const AuthProvider = ({ children }) => {
     try {
       const backend = getBackendUrl();
       const { data } = await axios.post(`${backend}/api/login`, {
-        username: email,
+        email,
         password
       });
 
